cdb95990673bc41b5024f1f27695ddd23fd336ab
johan <johanwirasugianto@gmail.com>
2023-11-06 19:52:49 +0700

    update Home and Login page

---
+++ resources/views/components/posts/post-card.blade.php
@@ -0,0 +1,21 @@
+@props(['post'])
+<div>
+    <a href="#">
+        <div>
+            <img class="w-full rounded-xl"
+                 src="{{ $post->image }}">
+        </div>
+    </a>
+    <div class="mt-3">
+        <div class="flex items-center mb-2">
+            <a href="#" class="bg-red-600
+                                        text-white
+                                        rounded-xl px-3 py-1 text-sm mr-3">
+                Laravel
+            </a>
+            <p class="text-gray-500 text-sm">{{ $post->published_at }}</p>
+        </div>
+        <a href="#" class="text-xl font-bold text-gray-900">{{ $post->titile }}</a>
+    </div>
+
+</div>
